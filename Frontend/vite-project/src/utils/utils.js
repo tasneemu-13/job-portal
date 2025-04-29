40b8e8611e0077d21efd2bc85aa7f057fa23b14b
Tasneem D <dewastasneem618@gmail.com>
2025-04-29 12:11:53 +0530

    update baseURL

--- Frontend/vite-project/src/utils/utils.js
+++ Frontend/vite-project/src/utils/utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({  // ✅ Use lowercase 'create'
-    baseURL: 'http://localhost:1234/api/v1' // ✅ Fixed baseURL format
+  //  baseURL: 'http://localhost:1234/api/v1' // ✅ Fixed baseURL format
+      baseURL : 'https://job-portal-qxix.onrender.com/api/v1', // ✅ Fixed baseURL format
 });
