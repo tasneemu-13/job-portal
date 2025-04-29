b6852b5ad31e2cc521a81b34fc2d4218b52a923d
Tasneem D <dewastasneem618@gmail.com>
2025-04-29 14:02:38 +0530

    Push MERN stack folder with all updates

--- Frontend/vite-project/eslint.config.js
+++ Frontend/vite-project/eslint.config.js
@@ -9,7 +9,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node, // ✅ Add this line to recognize `process`
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
